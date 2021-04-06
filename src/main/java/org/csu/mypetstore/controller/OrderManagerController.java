8279ace51d75c6671a51888d0a9ee1e33a51c6bc
破晨雲 <631499573@qq.com>
2021-04-06 22:43:34 +0800

    前端路径解决

--- src/main/java/org/csu/mypetstore/controller/OrderManagerController.java
+++ src/main/java/org/csu/mypetstore/controller/OrderManagerController.java
@@ -22,6 +22,6 @@ public class OrderManagerController
     @GetMapping("/view")
     public String viewOrders()
     {
-        return "/manager/orderManager";
+        return "/manager/manager";
     }
 }
