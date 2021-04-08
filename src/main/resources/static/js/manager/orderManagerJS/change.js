6429d1c8ab06c050f1091447bf1670b0b708752f
pochenyun72 <pochenyun@gmail.com>
2021-04-08 12:59:52 +0800

    查验

--- src/main/resources/static/js/manager/orderManagerJS/change.js
+++ src/main/resources/static/js/manager/orderManagerJS/change.js
@@ -1,9 +1,22 @@
+//编辑Order，把数据移到表中
 $(function ()
 {
     $(".Js_edit").click(function ()
     {
         var valueOfOrderId = $(this).children('#value').text();
-        console.log(valueOfOrderId);
-        $('input#editId').val(valueOfOrderId);
+
+        var parentTr = $(this).parent().parent();
+        var valueOfShipAddress1 = parentTr.children('#address').children('#shipAddress1').text();
+        var valueOfShipAddress2 = parentTr.children('#address').children('#shipAddress2').text();
+        var valueOfShipCity = parentTr.children('#address').children('#shipCity').text();
+        var valueOfShipZip = parentTr.children('#shipZip').text();
+        var valueOfCourier = parentTr.children('#courier').text();
+        console.log(valueOfOrderId  +" " + valueOfShipAddress1);
+        $('input#editOrderId').val(valueOfOrderId);
+        $('input#editShipAddress1').val(valueOfShipAddress1);
+        $('input#editShipAddress2').val(valueOfShipAddress2);
+        $('input#editShipCity').val(valueOfShipCity);
+        $('input#editShipZip').val(valueOfShipZip);
+        $('input#editCourier').val(valueOfCourier);
     });
 });
