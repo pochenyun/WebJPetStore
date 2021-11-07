db0315c489104eea1e152aa1f94b2b0fab22cfc3
pochenyun72 <pochenyun@gmail.com>
2021-11-07 17:33:00 +0800

    改HTML，重复的地方太多了

--- database/mypetstore.sql
+++ database/mypetstore.sql
@@ -244,7 +244,7 @@ DROP TABLE IF EXISTS `manager`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `manager` (
-  `ID` varchar(255) DEFAULT NULL,
+  `id` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
